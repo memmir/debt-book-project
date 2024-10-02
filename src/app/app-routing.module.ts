27e6ef91e8ab912df3a346e64f13198176e0bb7f
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-10-02 17:57:22 +0300

    Created route for customers endpoint and global css for container.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomersComponent} from "./components/customers/customers.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    component: CustomersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
