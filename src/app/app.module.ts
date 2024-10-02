3bf5ac964709abdf203955778ac54c62608b5ec6
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-10-02 17:51:25 +0300

    Created customers component to list the customers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import {environment} from "../environments/environment";
 import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
 import { NavigationComponent } from './components/navigation/navigation.component';
 import {MaterialModule} from "./material/material/material.module";
+import { CustomersComponent } from './components/customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
