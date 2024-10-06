c6ba9f259d8a191ee496382642a5e8741b2f91a1
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-10-07 00:08:33 +0300

    Created getAllCustomers method.

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FsApiService} from "../../services/fs-api.service";
+import {Customer} from "../../models/Customer";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {CustomerDialogComponent} from "../dialogs/customer-dialog/customer-dialog.component";
 
 @Component({
   selector: 'app-customers',
@@ -7,12 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
+  customers?: Customer[];
+
   dataSource:any;
   displayedColumns = ['CustomerName', 'CustomerPhone', 'Calculations']
 
-  constructor() { }
+
+
+  constructor(
+    private afs: FsApiService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  GetAllCustomers(): void {
+    this.afs.getAllCustomers().subscribe((data:any)=>{
+      this.customers = data.map(a=>{
+        return{
+          CustomerId:a.payload.doc.id,
+          ...a.payload.doc.data()
+        }as Customer
+      })
+      this.dataSource = new MatTableDataSource(this.customers);
+    })
   }
 
 }
